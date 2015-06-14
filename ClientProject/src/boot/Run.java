db13a4e1414966a2d06efde6b30e76516d05e1fd
eladdoo <eladdoocker1@gmail.com>
2015-06-14 18:33:48 +0300

    created socket to connect to server

--- ClientProject/src/boot/Run.java
+++ ClientProject/src/boot/Run.java
@@ -8,7 +8,7 @@ public class Run
 {
 
 	public static void main(String[] args) 
-	{
+	{//aas
 		MyClient c = new MyClient();
 		MyMazeWindow win = new MyMazeWindow("my maze",500,500);
 		Presenter p = new Presenter(c,win);
